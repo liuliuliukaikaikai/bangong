9b17e81013fdf105c23f257cdf234aa05af5ea02
SkyGlad <1109259152@qq.com>
2021-04-22 08:05:36 +0800

    application.yml配置完成

---
+++ bangong-server/src/main/java/org/hdxy/bangong/ServerApplication.java
@@ -0,0 +1,10 @@
+package org.hdxy.bangong;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+
+    }
+}
